eb479924dcbfd4330c8ac64fa32aec2ea987e643
Jason Pettett <jason.pettett@gmail.com>
2016-06-26 18:48:49 +1000

    Add signout functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 
 class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return <li className="nav-item">
+        <Link className="nav=link" to="/signout">Sign Out</Link>
+      </li>;
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav=link" to="/signin">Sign In</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav=link" to="/signup">Sign Up</Link>
+        </li>
+      ];
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Signin
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
