0a6963f1d0c9d9b1ba895accc0fba4dc2b4ddd83
Jason Pettett <jason.pettett@gmail.com>
2016-06-26 18:29:59 +1000

    Add login communication to backend including auth error handling

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
-
+import * as actions from '../../actions';
 
 class Signin extends Component {
   handleFormSubmit({email, password}) {
     console.log(email, password);
     // Need to do something to sign user in
+    this.props.signinUser({ email, password });
+  }
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
   }
 
   render () {
@@ -19,8 +29,9 @@ class Signin extends Component {
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input {...password} className="form-control" />
+          <input {...password} type="password" className="form-control" />
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">
           Sign in
         </button>
@@ -29,8 +40,12 @@ class Signin extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password']
-})(Signin);
+}, mapStateToProps, actions)(Signin);
