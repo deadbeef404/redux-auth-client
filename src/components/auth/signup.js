abeb08da2df291d5dc292c2985e987f7f03f5040
Jason Pettett <jason.pettett@gmail.com>
2016-06-27 18:01:59 +1000

    Add first iteration of validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -5,6 +5,7 @@ import * as actions from '../../actions';
 class Signup extends Component {
   render() {
     const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+
     return (
       <form>
         <fieldset className="form-group">
@@ -14,6 +15,7 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Password:</label>
           <input className="form-control" {...password} type="password" />
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm Password:</label>
@@ -25,7 +27,29 @@ class Signup extends Component {
   }
 }
 
+function validate(formProps) {
+  const errors = {};
+
+  if (!formProps.email) {
+    errors.email = 'Please enter an email';
+  }
+  if (!formProps.password) {
+    errors.password = 'Please enter an email';
+  }
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = 'Please enter an email';
+  }
+
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = "Passwords must match";
+  }
+  return errors;
+}
+
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
 })(Signup);
